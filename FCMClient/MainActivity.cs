76b7fad50951fe2f823b51fe3d08fd30fbf16e02
tiagoluzh607 <tiagoluz.h607@gmail.com>
2018-04-03 17:20:17 -0300

    Primeiro Commit

---
+++ FCMClient/MainActivity.cs
@@ -0,0 +1,69 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+using Firebase.Messaging;
+using Firebase.Iid;
+using Android.Util;
+using Android.Gms.Common;
+
+namespace FCMClient
+{
+    [Activity(Label = "FCMClient", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        TextView msgText;
+        const string TAG = "MainActivity";
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            
+
+            //Verifica se o google play service esta ativado e exibe no text view
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.Main);
+            msgText = FindViewById<TextView>(Resource.Id.msgText);
+
+            //Quando o usuario clica em cima da notificação esse código pega oque estava escrito na notificacao e exibe no console
+            if (Intent.Extras != null)
+            {
+                foreach (var key in Intent.Extras.KeySet())
+                {
+                    var value = Intent.Extras.GetString(key);
+                    Log.Debug(TAG, "Key: {0} Value: {1}", key, value);
+                }
+            }
+
+            IsPlayServicesAvailable();
+
+            //Mostra o Tolken qundo o botão é clicado
+            var logTokenButton = FindViewById<Button>(Resource.Id.logTokenButton);
+            logTokenButton.Click += delegate {
+                Log.Debug(TAG, "InstanceID token: " + FirebaseInstanceId.Instance.Token);
+            };
+        }
+
+        //Verifica se o play services esta instalado
+        public bool IsPlayServicesAvailable()
+        {
+            int resultCode = GoogleApiAvailability.Instance.IsGooglePlayServicesAvailable(this);
+            if (resultCode != ConnectionResult.Success)
+            {
+                if (GoogleApiAvailability.Instance.IsUserResolvableError(resultCode))
+                    msgText.Text = GoogleApiAvailability.Instance.GetErrorString(resultCode);
+                else
+                {
+                    msgText.Text = "This device is not supported";
+                    Finish();
+                }
+                return false;
+            }
+            else
+            {
+                msgText.Text = "Google Play Services is available.";
+                return true;
+            }
+        }
+    }
+}
+
---
+++ FCMClient/obj/Debug/android/src/md59392ef1ee1f1792b4e65153d4d955825/MyFirebaseIIDService.java
@@ -0,0 +1,47 @@
+package md59392ef1ee1f1792b4e65153d4d955825;
+
+
+public class MyFirebaseIIDService
+	extends com.google.firebase.iid.FirebaseInstanceIdService
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onTokenRefresh:()V:GetOnTokenRefreshHandler\n" +
+			"";
+		mono.android.Runtime.register ("FCMClient.MyFirebaseIIDService, FCMClient, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", MyFirebaseIIDService.class, __md_methods);
+	}
+
+
+	public MyFirebaseIIDService ()
+	{
+		super ();
+		if (getClass () == MyFirebaseIIDService.class)
+			mono.android.TypeManager.Activate ("FCMClient.MyFirebaseIIDService, FCMClient, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onTokenRefresh ()
+	{
+		n_onTokenRefresh ();
+	}
+
+	private native void n_onTokenRefresh ();
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
